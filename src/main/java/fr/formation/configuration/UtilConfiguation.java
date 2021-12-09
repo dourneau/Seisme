1386bbe0b266ae6986c97e4aca5f1804a4316b4c
Didier <didier.dourneau@yahoo.com>
2021-12-09 10:14:48 +0100

    Version 2.0: gestion de SwaggerConfiguration UtilConfiguration

---
+++ src/main/java/fr/formation/configuration/UtilConfiguation.java
@@ -0,0 +1,14 @@
+package fr.formation.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UtilConfiguation {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
